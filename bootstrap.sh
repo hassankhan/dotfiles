b18e9db5a06676b9144962725571da3a5e0304dc
Hassan Khan <hassan@radicalcondor.dev>
2021-11-21 03:43:09 +0000

    feat: replace `bash` with `zsh`, remove unused configurations

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,20 +4,7 @@ cd "$(dirname "${BASH_SOURCE}")";
 
 git pull origin master;
 
-BREW_PREFIX=$(brew --prefix)
-
 function doIt() {
-
-    installation_types=("personal" "work")
-    read -p 'Is this being installed on a personal or work machine: ' INSTALLATION_TYPE;
-
-    # If unrecognised installation type
-    if [[ ! " ${installation_types[@]} " =~ " ${INSTALLATION_TYPE} " ]]; then
-        echo "Installation type must be one of: personal, work";
-        exit 1;
-    fi
-
-    cp Brewfile-${INSTALLATION_TYPE} Brewfile;
 
     # Copy files to Home folder
     rsync --exclude ".git/" \
@@ -26,21 +13,15 @@ function doIt() {
         --exclude "dev.sh" \
         --exclude "README.md" \
         --exclude "LICENSE" \
-        --exclude "Brewfile-*" \
+        --exclude ".macos" \
         -avh --no-perms . ~;
 
     # Change current working directory to Home folder
     cd ~;
     # Install apps/utilities from Homebrew
     brew bundle;
-    # Set Homebrew-installed Bash as default
-    if ! fgrep -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
-        echo "${BREW_PREFIX}/bin/bash" | sudo tee -a /etc/shells;
-        chsh -s "${BREW_PREFIX}/bin/bash";
-    fi;
-
-    source ~/.bash_profile;
-    ./.macos;
+    # Set macOS defaults
+    # ./.macos;
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then

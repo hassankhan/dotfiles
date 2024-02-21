56efaf39e77933a6ed07e56132b9911032170149
Hassan Khan <contact@hassankhan.me>
2024-02-21 09:01:46 +0000

    fix(config/iterm): correctly source iTerm2 integration

--- stow/fish/.config/fish/config.fish
+++ stow/fish/.config/fish/config.fish
@@ -4,13 +4,10 @@ fenv source ~/.profile
 # Commands to run in interactive sessions can go here
 if status is-interactive
 
-    # iTerm2 integration
-    test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
-
     # Input customizations/enhancements
     test -e {$HOME}/.config/fish/conf.d/abbr.fish ; and source {$HOME}/.config/fish/conf.d/abbr.fish
     test -e {$HOME}/.config/fish/conf.d/aliases.fish ; and source {$HOME}/.config/fish/conf.d/aliases.fish
-    test -e {$HOME}/.config/fish/conf.d/paths.fish ; and source {$HOME}/.config/fish/conf.d/paths.fish
+    test -e {$HOME}/.config/fish/conf.d/iterm2.fish ; and source {$HOME}/.config/fish/conf.d/iterm2.fish
     test -e {$HOME}/.config/fish/conf.d/utils.fish ; and source {$HOME}/.config/fish/conf.d/utils.fish
     
     # Shell customizations
